86c3234ba321d7ba04b9b653298d47809cbae1ff
DuArms <58860510+DuArms@users.noreply.github.com>
2021-03-07 18:14:57 +0200

    Delete Tema_2/dist directory

--- Tema_2/dist/const/html_pages.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.home_page = void 0;
-const fs = require("fs");
-const home_page = fs.readFileSync("./lib/html/home.html");
-exports.home_page = home_page;
-//# sourceMappingURL=html_pages.js.map
