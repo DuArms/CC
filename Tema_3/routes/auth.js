3bfe02e6b29873d661765a2ca67a5a01dcba0746
DuArms <58860510+DuArms@users.noreply.github.com>
2021-03-29 20:16:39 +0300

    this works

--- Tema_3/routes/auth.js
+++ Tema_3/routes/auth.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const { authController } = require('../controllers');
 
 router.get('/',(req,res) =>{
-    res.send({status:'MERGE FRATELE DIN CER'});
+    res.send({status:'MERGE FRATELE DIN CER 2'});
 })
 
 module.exports = router;
